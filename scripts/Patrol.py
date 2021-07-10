4518cb7fc5975c2b0695b677efbc25af8c6646fa
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2021-07-10 18:06:08 +0800

    更改速度调整方式

--- scripts/Patrol.py
+++ scripts/Patrol.py
@@ -28,9 +28,6 @@ class Patrol:
         self.paramEntries = [Entry(self.paramFrame, textvariable=self.paramVars[i], validate="focusout", validatecommand=lambda i=i: self.paramCallback(i), width=8) for i in range(len(self.NAMES))]
 
         self.controlFrame = LabelFrame(self.root, text="控制")
-        self.speedLabel = Label(self.controlFrame, text="速度")
-        self.speedVar = StringVar(value="0")
-        self.speedEntry = Entry(self.controlFrame, textvariable=self.speedVar, validate="focusout", validatecommand=self.speedCallback, width=6)
         self.uploadButton = Button(self.controlFrame, text="上传", state="disabled", command=self.upload)
         self.startButton = Button(self.controlFrame, text="启动", state="disabled", command=self.start)
         self.resetButton = Button(self.controlFrame, text="重置", state="disabled", command=self.reset)
@@ -41,8 +38,6 @@ class Patrol:
             self.paramEntries[i].grid(row=i, column=1, padx=3, pady=3)
 
         self.controlFrame.pack(side="left", fill="y", padx=3, pady=3)
-        self.speedLabel.grid(row=0, column=0, padx=3, pady=3)
-        self.speedEntry.grid(row=0, column=1, padx=3, pady=3)
         self.uploadButton.grid(row=1, column=0, columnspan=2, padx=3, pady=1)
         self.startButton.grid(row=2, column=0, columnspan=2, padx=3, pady=1)
         self.resetButton.grid(row=3, column=0, columnspan=2, padx=3, pady=1)
@@ -65,15 +60,6 @@ class Patrol:
             return False
         return True
 
-    def speedCallback(self):
-        try:
-            t = int(self.speedVar.get())
-            if -32768 <= t < 32768:
-                return True
-        except:
-            self.speedVar.set("0")
-        return False
-
     def setActivate(self, activate: bool):
         state = ("disabled", "normal")
         self.uploadButton["state"] = self.startButton["state"] = self.resetButton["state"] = state[activate]
@@ -87,7 +73,6 @@ class Patrol:
     def start(self):
         for i in range(5):
             self.main.setstate.checked[i].set(True)
-        self.main.write(self.main.setstate.PWM + b"\x04" + int(self.speedVar.get()).to_bytes(2, "little", signed=True))
         self.main.setstate.uploadState()
 
     def reset(self):
