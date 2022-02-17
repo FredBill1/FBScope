6fd5184ec4979c7d2d4620fdc9205cba5886f544
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2022-02-17 22:27:33 +0800

    弄了个float entry

---
+++ src/FBPanel/FBWidgets/FBFloatEntry.py
@@ -0,0 +1,27 @@
+from .FBWidget import FBWidget
+import ttkbootstrap as ttk
+import tkinter as tk
+import os.path, sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
+from utils import ValEntry
+
+
+class FBFloatEntry(FBWidget):
+    def construct(self, frame: ttk.Frame) -> None:
+        self.data.setdefault("text", tk.StringVar(value="0.0"))
+        self.label = ttk.Label(frame, text=self.name)
+        self.entry = ValEntry(ValEntry.type_validator(float), frame, textvariable=self.data["text"])
+        self.entry.bind("<Return>", lambda _: self._callback("enter"))
+        self.label.pack(side="left")
+        self.entry.pack(side="left")
+
+    def applyConfig(self):
+        self.entry["width"] = int(self.config.setdefault("宽度", "20"))
+
+    def rename(self):
+        super().rename()
+        self.label["text"] = self.name
+
+
+__all__ = ["FBFloatEntry"]
--- src/FBPanel/FBWidgets/__init__.py
+++ src/FBPanel/FBWidgets/__init__.py
@@ -3,6 +3,7 @@ from typing import Dict, Any, List
 from .FBWidget import FBWidget
 from .FBButton import FBButton
 from .FBEntry import FBEntry
+from .FBFloatEntry import FBFloatEntry
 
-FBWIDGET_LIST: List[FBWidget] = [FBButton, FBEntry]
+FBWIDGET_LIST: List[FBWidget] = [FBButton, FBEntry, FBFloatEntry]
 FBWIDGET_DICT: Dict[Any, FBWidget] = {cls.__name__: cls for cls in FBWIDGET_LIST}
