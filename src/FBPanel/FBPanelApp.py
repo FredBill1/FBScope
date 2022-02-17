537cf18f48a9f8bdc7801ba25bcf57bb15e58603
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2022-02-17 13:37:15 +0800

    FBPanel也加了isServer选项

--- src/FBPanel/FBPanelApp.py
+++ src/FBPanel/FBPanelApp.py
@@ -5,14 +5,14 @@ import json
 import tkinter as tk
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-from FBSocket import FBClient
+from FBSocket import FBClient, FBServer
 
 CFG_DIR = os.path.expanduser("~/.FBScope")
 CFG_PATH = os.path.join(CFG_DIR, "FBPanel.json")
 
 
 class FBPanelApp:
-    def __init__(self, cfg: dict = None):
+    def __init__(self, cfg: dict = None, isServer: bool = False):
         os.makedirs(CFG_DIR, exist_ok=True)
         if cfg is None and os.path.exists(CFG_PATH):
             with open(CFG_PATH, "r") as f:
@@ -20,7 +20,7 @@ class FBPanelApp:
         if cfg is None:
             cfg = {"canvases": []}
 
-        self.uartClient = FBClient()
+        self.uartClient = FBServer() if isServer else FBClient()
         self.uartClient.start()
 
         style = Style("cosmo")
@@ -31,7 +31,6 @@ class FBPanelApp:
 
         self.root.geometry(cfg.get("geometry", "400x300+30+30"))
         self.root.protocol("WM_DELETE_WINDOW", self.onClose)
-
 
         self.mainloop = self.root.mainloop
 
