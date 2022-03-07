cd7fb58701a6b296291128d4a6b59bd0f133c18e
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2022-03-07 15:35:26 +0800

    launcher弄了自动多行

--- src/launcher.py
+++ src/launcher.py
@@ -2,6 +2,7 @@ import ttkbootstrap as ttk
 from os.path import join, dirname
 from sys import executable
 from subprocess import Popen, DETACHED_PROCESS
+from math import sqrt, floor, ceil
 
 DIR = dirname(__file__)
 APPs = ["FBSerial", "FBPlot", "FBPanel", "FBRot", "FBImg", "FBRecorder"]
@@ -11,11 +12,12 @@ if __name__ == "__main__":
     root = style.master
     root.title("launcher")
     root.resizable(False, False)
-    for app in APPs:
+    C = ceil(len(APPs) / floor(sqrt(len(APPs))))
+    for i, app in enumerate(APPs):
         ttk.Button(
             root,
             text=app,
             command=lambda app=app: Popen([executable, join(DIR, app, "main.py")], creationflags=DETACHED_PROCESS),
-        ).pack(side="left", padx=5, pady=5, fill="both", expand=True)
+        ).grid(row=i // C, column=i % C, padx=5, pady=5, sticky="nsew")
     root.mainloop()
 
